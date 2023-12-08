df0a73c1fbed451c22d3ef5bdfad3e0f34ee4082
Arthanarieswaran <arthanarieswaran2002sn@gmail.com>
2023-12-08 18:29:47 +0530

    thired

--- resources/views/frontend/pages/register.blade.php
+++ resources/views/frontend/pages/register.blade.php
@@ -1,5 +1,20 @@
 @extends('frontend.layouts.master')
 
 @section('main-content')
-register part
+    <!-- goto home button and spotted place name -->
+    <div class="breadcrumbs  bg-secondary" style="height: 7vh;">
+        <div class="container">
+            <div class="row ">
+                <div class="col-12">
+                    <div class="bread-inner">
+                        <ul class="bread-list d-flex flex-row mt-3 ">
+                            <li><a href="{{route('home')}}">Home<i class="fa-solid fa-arrow-right ms-2"></i></a></li>
+                            <li class="active ms-2"><a href="javascript:void(0);">Register</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End goto home button and spotted place name -->
 @endsection
